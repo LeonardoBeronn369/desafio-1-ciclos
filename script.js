02027d5acc326b5a3ea836635a141cbcbb5accc7
leonardo Beron <leonaroberon33@gmail.com>
2022-07-28 21:39:33 -0300

    desafio ciclo

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//  Ejemplo Uno
+
+let saludo = parseInt(prompt("Hola: Ingrese un numero!!"))
+
+for(let i = 0; i <= saludo; i = i + 1) {
+      console.log("Hola", i)
+}
+
+
+
+
+//  Ejemplo dos
+
+// let color = "";
+// while(color != "negro"){
+//      color = prompt("¿Cual es mi color preferido?");
+//      console.log("Color: " + color);
+// }
+    
